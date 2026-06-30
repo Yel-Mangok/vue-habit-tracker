79119cb0a3a1160785ef9f6cbe7182981bd9a17a
YEL MANGOK JOHN <yelmangok1992@gmail.com>
2026-06-30 16:03:24 +0200

    Main module updated

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
+//import the vue.js function to create the application
 import { createApp } from 'vue'
-
+//import the main application vue.js module
 import App from './App.vue'
+//import the custom vue.js application router
+import router from "./router/index.js";
 
-createApp(App).mount('#app')
+createApp(App).use(router).mount('#app')
